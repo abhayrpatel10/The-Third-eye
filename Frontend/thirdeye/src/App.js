bbdff907d9da4bac35acbe2bb9f9094a8f6675c0
YashBoss <yashksri199747@gmail.com>
2019-10-19 18:46:39 +0530

    frontend

---
+++ Frontend/thirdeye/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import WebcamCapture from "./Components/WebcamCapture";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Third Eye</h1>
+      <WebcamCapture />
+    </div>
+  );
+}
+
+export default App;
