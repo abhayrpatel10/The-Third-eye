9bcbff32e536d2e40dfb72696ead9c89cde80b4b
abhayrpatel10 <abhayrpatel10@gmail.com>
2019-10-19 19:42:23 +0530

    bug fixes

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const multipart = require('connect-multiparty');
 const cloudinary = require('cloudinary');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const vehcilerouter=require('./routes/addvehicles')
-
-const check=require('./routes/checkforviolations')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(vehcilerouter)
-
-app.use(check)
 app.use(cors());
+//app.use(fileUpload())
+
+port=process.env.PORT||3000
 
 const multipartMiddleware = multipart();
 
@@ -23,20 +20,16 @@ cloudinary.config({
     api_secret: 'RSe-sJZxufTtmGnncSIscE3DugA'
 });
 
-
 app.post('/upload', multipartMiddleware, function(req, res) {
     cloudinary.v2.uploader.upload(req.files.image.path,
       {
         ocr: "adv_ocr"
       }, function(error, result) {
           if( result.info.ocr.adv_ocr.status === "complete" ) {
-            res.json(result.info.ocr.adv_ocr.data[0].textAnnotations[0].description); 
+            res.json(result.info.ocr.adv_ocr.data[0].textAnnotations[0].description); // result.info.ocr.adv_ocr.data[0].textAnnotations[0].description (more specific)
           }
       });
-    });
-
-
-port=process.env.PORT||3000
+  });
 
 app.listen(port,()=>{
     console.log("Listening on port "+port)
