9225e6d2967ec4384128cd1d65c58d41367fe292
abhayrpatel10 <abhayrpatel10@gmail.com>
2019-10-19 19:24:44 +0530

    bug fixes

--- routes/checkforviolations.js
+++ routes/checkforviolations.js
@@ -2,16 +2,46 @@ const express=require('express')
 require('../db/mongoose')
 app=express.Router()
 const Vehicle=require('../models/vehicle')
+const Nexmo = require('nexmo');
+
+
+
+
+function sendmessage(mobileno,amount){
+    const nexmo = new Nexmo({
+        apiKey: 'e99d0ebb',
+        apiSecret: 'SCEmJzV4we0tHi9J',
+      });
+      
+      const from = 'Nexmo';
+      const to = '919952121766';
+      const text = 'You have violated traffic rules these';
+
+      nexmo.message.sendSms(from, to, text);
+
+}
 
 
 app.post('/check',async(req,res)=>{
    
-    var veh=await Vehicle.findOne({vehicleno:req.body.vehicleno})
+    try{
+        var veh=await Vehicle.findOne({vehicleno:req.body.vehicleno})
     if(veh.record===[]){
         res.send('No records found for this vehicle')
     }else{
-        res.send(veh.record)
+        const listval=Object.values(veh.record[0])
+        sum=0
+        for(var i=0;i<listval.length;i++){
+            sum=sum+listval[i]
+        }
+
+        res.send(sum.toString())
     }
+
+    }catch(e){
+        res.send('Data for the vehicle not found')
+    }
+    
 
 })
 
